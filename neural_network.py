8716d5c133077f710b394251029d85a38e8bd402
elemyoav <elemyoav@gmail.com>
2024-01-28 20:14:21 +0200

    added documentation to neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -3,28 +3,69 @@ from layers import ReLULayer, TanhLayer, ResidualReLULayer, ResidualTanhLayer
 from losses import SoftmaxLayer
 import scipy.io
 import matplotlib.pyplot as plt
-from utils import plot_data, plot_loss_and_accuracy, SGD
+from utils import plot_data, plot_loss_and_accuracy, SGD, NetworkGradientTest
 LR = 5e-5
 
 
 class GenericNetwork:
+    """
+    Generic neural network class, can be used to create any fully connected neural network.
+
+    Parameters:
+    output_layer: the output layer of the network, should be either SoftmaxLayer or LinearLayer
+    layers: a list of layers, each layer should be an extention of HiddenLayer
+    """
+
     def __init__(self, output_layer, layers=[]):
         self.layers = layers
         self.output_layer = output_layer
-        self.cache = []
+        self.cache = [] # cache for forward pass
 
-    def loss(self, X, C):
+    def loss(self, X, C, clear_cache=True):
+        """
+        Computes the loss of the network on a given batch of data
+
+        Parameters:
+        X is a matrix of size (input_size, batch_size)
+        C is a matrix of size (output_size, batch_size)
+        clear_cache: if True, clears the cache after computing the loss
+
+        Returns:
+        a scalar loss
+        """
+
         self.forward(X)
         loss = self.output_layer.loss(self.cache[-1], C)
-        self.clear_cache()
+        if clear_cache: self.clear_cache()
         return loss
     
     def accuracy(self, X, C):
+        """
+        Computes the accuracy of the network on a given batch of data
+
+        Parameters:
+        X is a matrix of size (input_size, batch_size)
+        C is a matrix of size (output_size, batch_size)
+
+        Returns:
+        a scalar accuracy
+        """
+
         output = self.forward(X)
         self.clear_cache()
         return np.mean(np.argmax(output.T, axis=0) == np.argmax(C, axis=0))
     
     def forward(self, X):
+        """
+        Computes the output of the network on a given batch of data
+
+        Parameters:
+        X is a matrix of size (input_size, batch_size)
+
+        Returns:
+        a matrix of size (output_size, batch_size)
+        """
+
         self.cache = [X]
         for layer in self.layers:
             X = layer.forward(X)
@@ -34,19 +75,40 @@ class GenericNetwork:
         return Y
 
     def clear_cache(self):
+        """
+        Clears the cache of the network
+        """
+
         self.cache = []
     
     def backpropagation(self, X, Y):
+        """
+        Computes the gradients of the network using backpropagation
+
+        Parameters:
+        X is a matrix of size (input_size, batch_size)
+        Y is a matrix of size (output_size, batch_size)
+
+        Returns:
+        a list of gradients, each gradient is a tuple (dW, db)
+        """
+
+        # initialize the list of gradients
         gradients = []
 
-        output = self.forward(X)
+        # preform a forward pass to fill the cache
+        self.forward(X)
+
+        # compute the gradient of the loss with respect to the output of the network
         output = self.cache[-1]
         dx = self.output_layer.grad_x(output, Y)
-
         dW = self.output_layer.grad_w(output, Y)
         db = self.output_layer.grad_b(output, Y)
+
+        # add the gradient of the output layer to the list of gradients
         gradients.append((dW, db))
 
+        # backpropagate the gradient for each layer in reverse order
         for i, layer in reversed(list(enumerate(self.layers))):
             dW = layer.JacTWMv(self.cache[i], dx)
 
@@ -54,17 +116,89 @@ class GenericNetwork:
 
             dx = layer.JacTxMv(self.cache[i], dx)
 
+            # add the gradient of the layer to the list of gradients
             gradients.append((dW, db))
+
+        # reverse the list of gradients and return it
         gradients.reverse()
         return gradients
 
     def update_parameters(self, gradients, lr):
+        """
+        Updates the parameters of each layer of the network using the learning rate
+
+        Parameters:
+        gradients: a list of gradients, each gradient is a tuple (dW, db), corresponding to a layer
+        lr: the learning rate
+
+        Returns:
+        None
+        """
+
         Θ, gradients = gradients[-1], gradients[:-1]
         self.output_layer.update_weights(Θ, lr)
 
         for i, Θ in enumerate(gradients):
             self.layers[i].update_weights(Θ, lr)
 
+    def loss_Θ(self, x, y, dΘ=None):
+        """
+        Computes the loss of the network on a given batch of data
+        when nudging the parameters by dΘ, that is we move all the parameters in the network
+
+        Parameters:
+        x is a matrix of size (input_size, batch_size)
+        y is a matrix of size (output_size, batch_size)
+        dΘ is a vector of size (total number of parameters, 1), if None, we compute the loss without nudging the parameters
+
+        Returns:
+        a scalar loss
+        """
+
+        # in the case where we don't move the parameters
+        if dΘ is None:
+            return self.loss(x, y)
+        
+        self.cache = [x]
+        start = 0
+
+        # moves each layer by the corresponding part of dΘ
+        for layer in self.layers:
+            end = start + layer.W.size + layer.b.size + x.size
+            x = layer.forward_Θ(x, dΘ[start:end])
+            self.cache.append(x)
+            start = end
+
+        return self.output_layer.loss_Θ(x, y, dΘ[start:])
+    
+    def grad_Θ(self, x, y):
+        """
+        Computes the gradient of the loss of the loss function of the network
+        with respect to all the parameters of the network, vecotrized.
+
+        we use the same idea of backpropagation.
+
+        Parameters:
+        x is a matrix of size (input_size, batch_size)
+        y is a matrix of size (output_size, batch_size)
+
+        Returns:
+        a vector of size (total number of parameters, 1)
+        """
+        
+        grads = []
+        self.loss_Θ(x, y)
+        grad = self.output_layer.grad_Θ(self.cache[-1], y)
+        dx = self.output_layer.grad_x(self.cache[-1], y)
+        grads.append(grad)
+        for i, layer in reversed(list(enumerate(self.layers))):
+            grad = layer.JacΘTMv(self.cache[i], dx)
+            grads.append(grad)
+            dx = layer.JacTxMv(self.cache[i], dx)
+
+        self.clear_cache()
+        grads.reverse()
+        return np.vstack(grads)
 
 
 class ResidualNeuralNetwork:
@@ -148,16 +282,16 @@ if __name__ == '__main__':
     # Dummy data (replace with real data)
     # Example usage with a small neural network
     layer1 = ReLULayer(2, 16)  # Example sizes
-    layer2 = ReLULayer(16, 128)
-    layer3 = ReLULayer(128, 512)
-    loss_layer = SoftmaxLayer(512, 2)
+    # layer2 = ResidualReLULayer(2, 128)
+    # layer3 = ResidualReLULayer(2, 512)
+    loss_layer = SoftmaxLayer(16, 2)
 
     NN = GenericNetwork(
         loss_layer,
         [
             layer1,
-            layer2,
-            layer3
+            # layer2,
+            # layer3
         ]
     )
     swissroll = scipy.io.loadmat('HW1_Data(1)/SwissRollData.mat')
@@ -166,10 +300,8 @@ if __name__ == '__main__':
     Xv = swissroll['Yv']
     Yv = swissroll['Cv']
 
-    # Train the network
-    tloss, vloss, tacc, vacc = SGD(Xt, Yt, Xv, Yv, NN, lr=LR, epochs=1000, batch_size=32)
-
-    plot_loss_and_accuracy(tloss, tacc, vloss, vacc)
-    plot_data(NN, Xt, Yt, Xv, Yv)
+    x = Xt[:, 0].reshape(-1, 1)
+    y = Yt[:, 0].reshape(-1, 1)
+    NetworkGradientTest(NN, x, y)
 
 
